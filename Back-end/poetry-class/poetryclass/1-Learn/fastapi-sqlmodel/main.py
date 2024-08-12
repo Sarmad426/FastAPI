23e2b443b6ab3bdb337edb0ce0aa2a68d414ee1b
Sarmad Rafique <sarmadrafique040@gmail.com>
2024-08-12 12:10:40 +0500

    Models with relationships

--- Back-end/poetry-class/poetryclass/1-Learn/fastapi-sqlmodel/main.py
+++ Back-end/poetry-class/poetryclass/1-Learn/fastapi-sqlmodel/main.py
@@ -58,6 +58,13 @@ class HeroUpdate(SQLModel):
     age: int | None = None
     team_id: int | None = None
 
+class HeroPublicWithTeam(HeroPublic):
+    team: TeamPublic | None = None
+
+
+class TeamPublicWithHeroes(TeamPublic):
+    heroes: list[HeroPublic] = []
+
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
@@ -108,7 +115,7 @@ def read_heroes(
     return heroes
 
 
-@app.get("/heroes/{hero_id}", response_model=HeroPublic)
+@app.get("/heroes/{hero_id}", response_model=HeroPublicWithTeam)
 def read_hero(*, session: Session = Depends(get_session), hero_id: int):
     hero = session.get(Hero, hero_id)
     if not hero:
@@ -161,7 +168,7 @@ def read_teams(
     return teams
 
 
-@app.get("/teams/{team_id}", response_model=TeamPublic)
+@app.get("/teams/{team_id}", response_model=TeamPublicWithHeroes)
 def read_team(*, team_id: int, session: Session = Depends(get_session)):
     team = session.get(Team, team_id)
     if not team:
