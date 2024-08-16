1ffa8be3f37b85b0ba69d3915445623a2f729fc2
Sarmad Rafique <sarmadrafique040@gmail.com>
2024-08-16 11:18:21 +0500

    Test FastAPI application

--- Back-end/poetry-class/poetryclass/1-Learn/fastapi-sqlmodel/test_main.py
+++ Back-end/poetry-class/poetryclass/1-Learn/fastapi-sqlmodel/test_main.py
@@ -5,19 +5,36 @@ Testing FastAPI API endpoints using pytest
 from fastapi.testclient import TestClient
 from sqlmodel import Session, SQLModel, create_engine
 
-from .main import app, get_session  
+from main import app
+from db_connection import get_session
 
+
+TEST_DB_URL = 'sqlite:///testing.db'
 
 def test_create_hero():
-        client = TestClient(app)  
+    engine = create_engine(  
+        TEST_DB_URL, connect_args={"check_same_thread": False}
+    )
+    SQLModel.metadata.create_all(engine)  
 
-        response = client.post(  
+    with Session(engine) as session:  
+
+        def get_session_override():
+            return session  
+
+        app.dependency_overrides[get_session] = get_session_override
+        
+
+        client = TestClient(app)
+
+        response = client.post(
             "/heroes/", json={"name": "Deadpond", "secret_name": "Dive Wilson"}
         )
-        data = response.json()  
+        app.dependency_overrides.clear()
+        data = response.json()
 
-        assert response.status_code == 200  
-        assert data["name"] == "Deadpond"  
-        assert data["secret_name"] == "Dive Wilson"  
-        assert data["age"] is None  
-        assert data["id"] is not None  
+        assert response.status_code == 200
+        assert data["name"] == "Deadpond"
+        assert data["secret_name"] == "Dive Wilson"
+        assert data["age"] is None
+        assert data["id"] is not None
