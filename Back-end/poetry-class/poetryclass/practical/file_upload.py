e6ddb462d04b7e8e766e9d1cfd3272a0ff84d68a
Sarmad Rafique <sarmadrafique040@gmail.com>
2024-08-20 22:55:09 +0500

    Request files Fast API docs practical

---
+++ Back-end/poetry-class/poetryclass/practical/file_upload.py
@@ -0,0 +1,19 @@
+"""
+Fast Api file upload
+"""
+
+from typing import Annotated
+
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+
+@app.post("/files/")
+async def create_file(file: Annotated[bytes, File(description="A File read as bytes")]):
+    return {"file_size": f"{len(file)} bytes"}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: Annotated[UploadFile, File(description="A File read as bytes")]):
+    return {"filename": file.filename,"content_type":file.content_type}
