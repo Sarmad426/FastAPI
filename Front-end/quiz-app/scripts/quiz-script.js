5ab36852b9468fbf66955414771142825fae9090
Sarmad Rafique <sarmadrafique040@gmail.com>
2024-08-28 15:19:39 +0500

    Updated front end UX of the quiz app

--- Front-end/quiz-app/scripts/quiz-script.js
+++ Front-end/quiz-app/scripts/quiz-script.js
@@ -2,7 +2,6 @@ const quizContainer = document.getElementById('quiz-container');
 const questionContainer = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const optionsContainer = document.getElementById('options');
-const nextButton = document.getElementById('next-btn');
 const resultContainer = document.getElementById('result-container');
 const scoreElement = document.getElementById('score');
 
@@ -33,6 +32,14 @@ function showQuestion() {
         const button = document.createElement('button');
         button.innerText = option;
         button.className = 'block w-full bg-gray-200 text-left py-2 px-4 rounded transition-all duration-300';
+        button.onmouseover = () =>{
+            button.classList.remove('bg-gray-200')
+            button.className +=' bg-[#8f94fb] text-white'
+        }
+        button.onmouseleave = () =>{
+            button.classList.remove('bg-[#8f94fb]','text-white')
+            button.className +=' bg-gray-200'
+        }
         button.onclick = (e) => {
             e.preventDefault()
             button.classList.remove('bg-gray-200')
@@ -44,24 +51,22 @@ function showQuestion() {
 }
 
 async function selectOption(selectedIndex) {
-    console.log('New data here')
     const questionData = quizData[currentQuestionIndex];
     if (selectedIndex === questionData.correct_option) {
-        const url = 'http://127.0.0.1:8000/points/add';
-        await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-    });
         score++;
     }
     currentQuestionIndex++;
-    nextButton.classList.remove('hidden');
-    optionsContainer.querySelectorAll('button').forEach(button => button.disabled = true);
+    showQuestion()
 }
 
 async function showResults() {
+    const url = `http://127.0.0.1:8000/points/update/${score}`;
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+});
     questionContainer.classList.add('hidden');
     resultContainer.classList.remove('hidden');
     scoreElement.innerText = `Your score is ${score} out of ${quizData.length}`;
@@ -73,9 +78,6 @@ function restartQuiz() {
     fetchQuizData();
 }
 
-nextButton.onclick = () => {
-    nextButton.classList.add('hidden');
-    showQuestion();
-};
+
 
 fetchQuizData();
