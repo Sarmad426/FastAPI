e6d558d00f81ee065a9fe8f9b7f5e8b7d91f50ea
Sarmad Rafique <sarmadrafique040@gmail.com>
2024-08-28 13:54:08 +0500

    Auth app backend

---
+++ Back-end/poetry-class/poetryclass/auth-app/db.py
@@ -0,0 +1,26 @@
+from sqlmodel import SQLModel, create_engine,Field,Session
+
+DB_URL = "sqlite:///auth_app.db"
+
+
+engine=create_engine(DB_URL, echo=True)
+
+class User(SQLModel, table=True):
+    id:int = Field(default=None, primary_key=True)
+    name: str
+    email: str
+    password: str
+    role: str
+
+class UserCreate(SQLModel, table=False):
+    name: str
+    email: str
+    password: str
+    role: str
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
