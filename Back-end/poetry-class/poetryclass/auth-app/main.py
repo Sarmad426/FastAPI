c11fb99a1f09dd2686e8feb74e1c956840ce9ef4
Sarmad Rafique <sarmadrafique040@gmail.com>
2024-08-30 11:24:10 +0500

    Added specific origins for the CORS

--- Back-end/poetry-class/poetryclass/auth-app/main.py
+++ Back-end/poetry-class/poetryclass/auth-app/main.py
@@ -26,9 +26,13 @@ app = FastAPI(
     lifespan=lifespan
 )
 
+origins = [
+    'http://127.0.0.1:5500'
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
